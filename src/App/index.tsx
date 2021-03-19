f189d8ebad423f88a396de40ae9ea4f1616f13f0
self067 <seltor@mail.ru>
2021-03-19 19:50:16 +0300

    init: start rslang app

---
+++ src/App/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC, useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { AppStyled } from './styled';
+
+export const App: FC = () => {
+return <AppStyled />;
+};
