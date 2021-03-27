fe39c50939e183585c6da8953dd64720702f9f96
Rinat <r.nas9329@gmail.com>
2021-03-28 02:03:52 +0600

    feat: add words list component

---
+++ src/components/FontAwesomeIcons/FontAwesomeIcons.jsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp, faTimes } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+export const AudioIcon = <FontAwesomeIcon icon={faVolumeUp} size="xs" />;
+export const CloseIcon = <FontAwesomeIcon icon={faTimes} size="lg" />;
